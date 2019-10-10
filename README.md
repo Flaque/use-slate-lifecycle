8937492b12918bd38c9dab1472eefab10f2fa398
Flaque <evanjamesconrad@gmail.com>
2019-10-10 13:54:26 -0700

    Add use-slate-lifecycle

---
+++ README.md
@@ -0,0 +1,9 @@
+# use-slate-lifecycle [![License](https://img.shields.io/npm/l/use-slate-lifecycle.svg)](https://github.com/flaque/use-slate-lifecycle/blob/master/package.json)
+
+## Install
+
+```
+yarn add
+```
+
+## Usage
---
+++ src/index.ts
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { OnChangeFn, OnChangeParam } from "slate-react";
+import useTimeout from "@flaque/use-timeout";
+import { Value } from "slate";
+import { isEqual } from "lodash";
+
+export default function useSlateLifecycle({
+  onUserStartsTyping,
+  onUserStopsTyping,
+  timeout
+}: {
+  onUserStartsTyping: (change?: OnChangeParam) => any;
+  onUserStopsTyping: (change?: OnChangeParam) => any;
+  timeout?: number;
+}) {
+  const [isTyping, setIsTyping] = useState(false);
+  const [setTimeoutHook, clearTimeoutHook] = useTimeout();
+  const [recentValue, setRecentValue] = useState<Value>(null);
+
+  const onChange: OnChangeFn = change => {
+    if (!isTyping) {
+      // on the very first call of onChange don't do anything, just set the value and continue
+      if (!recentValue) {
+        setRecentValue(change.value);
+        return;
+      }
+
+      // If we're just focusing the editor, we don't need to do anything, that's not typing.
+      const ops = change.operations.toArray().map(e => e.get("type"));
+      if (isEqual(ops, ["set_selection"])) {
+        setRecentValue(change.value);
+        return;
+      }
+
+      onUserStartsTyping(change);
+      setIsTyping(true);
+      setRecentValue(change.value);
+      return;
+    }
+
+    clearTimeoutHook();
+
+    setTimeoutHook(() => {
+      onUserStopsTyping(change);
+      setIsTyping(false);
+    }, timeout || 500);
+  };
+
+  return [onChange];
+}
